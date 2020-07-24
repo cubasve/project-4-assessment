72260018ba2b8250cd7891615cb7d76d7c7afea8
cubasve <eva.cubasvasquez@gmail.com>
2020-07-24 18:49:38 -0400

    Updates

---
+++ precache-manifest.ed764693c1e48f6bef5eac93d5018416.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00169ba367264eddc5bcc94600e12cb7",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "7bd680ccd52600772e90",
+    "url": "/project-4-assessment/static/css/main.937ae98b.chunk.css"
+  },
+  {
+    "revision": "acc314c7a357911f8cd2",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bd680ccd52600772e90",
+    "url": "/project-4-assessment/static/js/main.8decebd4.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  }
+]);
